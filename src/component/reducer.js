e1ecfb3459d849c7c6b16760ccc64ed188cec17f
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2019-09-27 14:29:56 +0530

    Redux Store Update

--- src/component/reducer.js
+++ src/component/reducer.js
@@ -1,9 +1,15 @@
+const intialstate = {
+    redux: "", 
+    apifetch:"Which action hero will save us in future ? .. Find out in Redux tab"
+}
+
 const reducer = function(state, action){
     switch (action.type) {
         case "ATTACK" :
             state = action.payload;
             break;
         default :
+            state = intialstate;
             break;
     }
     return state;
--- src/component/store.js
+++ src/component/store.js
@@ -1,10 +1,6 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-var intialstate = {
-    redux: "", 
-    apifetch:"Which action hero will save us in future ? .. Find out in Redux tab"
-}
-const store = createStore(reducer, intialstate);
+const store = createStore(reducer);
 
 export default store;

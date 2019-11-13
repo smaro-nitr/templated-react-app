3d047ecd87f74e31ea4043c51c3402be03daadd0
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2019-11-13 12:35:11 +0530

    No drag feature

--- src/component/ReactGrid/index.js
+++ src/component/ReactGrid/index.js
@@ -3,21 +3,32 @@ import GridLayout from 'react-grid-layout';
 
 class ReactGrid extends Component {	
 	render() {
-		const heightZoom = 1.5;
+		const heightZoom = 1;
     var layout = [
       {i: 'a', x: 0, y: 0, w: 12, h: 4 * heightZoom, minW: 12, minH: 4 * heightZoom, maxW: 12},
       {i: 'b', x: 0, y: 5, w: 9, h: 4 * heightZoom, minW: 9, minH: 4 * heightZoom, maxW: 12},
       {i: 'c', x: 10, y: 5, w: 3, h: 2 * heightZoom, minW: 3, minH: 2 * heightZoom, maxW: 12},
       {i: 'd', x: 10, y: 7, w: 3, h: 2 * heightZoom, minW: 3, minH: 2 * heightZoom, maxW: 12},
-      {i: 'e', x: 0, y: 9, w: 12, h: 4 * heightZoom, minW: 12, minH: 4 * heightZoom, maxW: 12}
+      {i: 'e', x: 0, y: 9, w: 12, h: 4 * heightZoom, minW: 12, minH: 4 * heightZoom, maxW: 12},
+      {i: 'f', x: 0, y: 9, w: 12, h: 4 * heightZoom, minW: 12, minH: 4 * heightZoom, maxW: 12}
     ];
     return (
-      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1000} isDraggable>
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={36}
+        width={1100}
+        isDraggable
+        isResizable
+        draggableCancel={'.cancel-drag'}
+      >
         <div key="a" className="bg-grey">a</div>
         <div key="b" className="bg-grey">b</div>
         <div key="c" className="bg-grey">c</div>
         <div key="d" className="bg-grey">d</div>
         <div key="e" className="bg-grey">e</div>
+        <div key="f" className="bg-grey">f<div className="cancel-drag">Cant Drag Me ;)</div></div>
       </GridLayout>
     )
   }
